ffd8ba11f7a25cb3c819be4e1fef9b06e67e65c9
Sofia Consolmagno Fontes <sofiaconsolmagno@hotmail.com>
2024-06-06 17:22:03 -0300

    Edit  Checkbox

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -4,9 +4,10 @@ import styles from './Task.module.css'
 export function Task(){
   return(    
     <div className={styles.content}>
-      <label className={styles.taskControl}>
-        <input type="checkbox" />
-        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+      <label className={styles.container}>
+        <input type="checkbox"/>
+        <span className={styles.checkMark}></span>
+        <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
       </label>
       <button className={styles.delete}><Trash size={16} color='#808080'/></button>
     </div>
